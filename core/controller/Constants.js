ab33ad56e386067693f1d0820d836a6d88b5c980
Programmix <programmixdev@gmail.com>
2016-08-11 01:45:08 -0700

    Controller framework

---
+++ core/controller/Constants.js
@@ -0,0 +1,27 @@
+
+"use strict";
+
+const Constants = {
+  WSS_URL: "wss://something.com",
+
+  OPCodes: {
+    DISPATCH: 0,
+    IDENTIFY: 1,
+    CONNECTED: 2
+  },
+
+  Operations: {
+    AUDIO_PLAY: 0,
+    AUDIO_STOP: 1,
+    AUDIO_VOLUME: 2,
+    AUDIO_PAUSE: 3,
+    AUDIO_RESUME: 4
+  },
+
+  Events: {
+    AUDIO_ENDED: 0
+  }
+}
+
+
+module.exports = Constants;
--- core/controller/Controller.js
+++ core/controller/Controller.js
@@ -1,9 +1,121 @@
 
 "use strict";
 
+const WebSocket = require("ws");
+
+const Constants = require("./Constants");
+
+
 class Controller {
   constructor() {
+    this.websocket = null;
 
+    this.token = null;
+  }
+
+  connect() {
+    if (this.websocket) return;
+
+    this.websocket = new WebSocket(Constants.WSS_URL);
+
+    var self = this;
+    var ws = this.websocket;
+
+    ws.on("open", () => {
+      var data = {
+        op: Consants.OPCodes.IDENTIFY,
+        d: {
+          token: "magic"
+        }
+      };
+
+      self.send(data);
+    });
+
+    ws.on("message", (data) => {
+      self.processWebsocketMessage(data);
+    });
+
+    ws.on("close", () => {
+      self.disconnected();
+    });
+
+    ws.on("error", (err) => {
+      self.disconnected(err);
+    });
+  }
+
+  disconnected(err) {
+    if (err) {
+      console.log(err);
+    }
+
+    this.websocket = null;
+  }
+
+  send(data) {
+    var ws = this.websocket;
+    if (!ws) return;
+
+    ws.send(data);
+  }
+
+  processWebsocketMessage(e) {
+    const OPCodes = Constants.OPCodes;
+
+    const packet = JSON.parse(e);
+    const op = packet.op;
+    const data = packet.d;
+
+    switch (packet.op) {
+
+      case OPCodes.DISPATCH:
+        this.processWebsocketDispatch(packet);
+        break;
+
+      case OPCodes.CONNECTED:
+        this.token = data.token;
+        break;
+
+      default:
+        console.log(`Unknown op code: ${op}`);
+        break;
+
+    }
+  }
+
+  processWebsocketDispatch(packet) {
+    const Operations = Constants.Operations;
+
+    const type = packet.t;
+
+    switch (type) {
+
+      case Operations.AUDIO_PLAY:
+
+        break;
+
+      case Operations.AUDIO_STOP:
+
+        break;
+
+      case Operations.AUDIO_VOLUME:
+
+        break;
+
+      case Operations.AUDIO_PAUSE:
+
+        break;
+
+      case Operations.AUDIO_RESUME:
+
+        break;
+
+      default:
+        console.log(`Unknown packet type: ${type}`);
+        break;
+
+    }
   }
 }
 
